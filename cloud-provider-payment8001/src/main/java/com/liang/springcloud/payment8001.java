ccc8dfa6eb1d59d31b0833d386288a6a077bb509
liangzl14 <liangzl14@chinaunicom.cn>
2020-11-04 08:17:31 +0800

    创建父工程及payment工程

---
+++ cloud-provider-payment8001/src/main/java/com/liang/springcloud/payment8001.java
@@ -0,0 +1,12 @@
+package com.liang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class payment8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(payment8001.class, args);
+    }
+}
